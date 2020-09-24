06bd7c8682169d82b6555bc2bac7cb6174356f88
sabithanune12 <71686861+sabithanune12@users.noreply.github.com>
2020-09-24 12:21:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-tar czf easyio.tar.gz main.js package.json public LICENSE
-scp easyio.tar.gz nanogram@nanogram.io:~
-rm easyio.tar.gz
+tar czf NodeJs.tar.gz main.js package.json public LICENSE
+scp NodeJs.tar.gz nanogram@nanogram.io:~
+rm NodeJs.tar.gz
 
 ssh nanogram@nanogram.io << 'ENDSSH'
 pm2 stop all
 rm -rf easyio
 mkdir easyio
-tar xf easyio.tar.gz -C easyio
-rm easyio.tar.gz
-cd easyio 
+tar xf NodeJs.tar.gz -C easyio
+rm NodeJs.tar.gz
+cd NodeJs 
 npm i
 pm2 start all
 ENDSSH
